1be18f31d1d67967fba257454e77483733781ede
Matheus Ferro <matheusferroraimundo@gmail.com>
2022-03-05 18:27:53 -0300

    2.17 Desambiguacao de beans com @Primary

--- src/main/java/com/matheusfr/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/matheusfr/algafood/di/notificacao/NotificadorEmail.java
@@ -1,8 +1,10 @@
 package com.matheusfr.algafood.di.notificacao;
 
 import com.matheusfr.algafood.di.modelo.Cliente;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class NotificadorEmail implements Notificador {
 
--- src/main/java/com/matheusfr/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/matheusfr/algafood/di/service/AtivacaoClienteService.java
@@ -5,26 +5,23 @@ import com.matheusfr.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Objects;
 
 @Component
 public class AtivacaoClienteService {
 
     @Autowired(required = false)
-    private List<Notificador> notificadores;
+    private Notificador notificador;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        for (Notificador notificador : notificadores) {
-            if(Objects.isNull(notificador)) {
-                System.out.println("Não existe notificador, mas cliente foi ativado!");
-                continue;
-            }
-
-            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        if(Objects.isNull(notificador)) {
+            System.out.println("Não existe notificador, mas cliente foi ativado!");
+            return;
         }
+
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
 
 }
