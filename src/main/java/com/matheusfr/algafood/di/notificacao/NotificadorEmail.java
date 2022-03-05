b3f35f7e9b58fa9c1b574f331a50d9d6eff092be
Matheus Ferro <matheusferroraimundo@gmail.com>
2022-03-05 17:39:02 -0300

    2.15 Dependência opcional com @Autowired

--- src/main/java/com/matheusfr/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/matheusfr/algafood/di/notificacao/NotificadorEmail.java
@@ -1,29 +1,15 @@
 package com.matheusfr.algafood.di.notificacao;
 
 import com.matheusfr.algafood.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class NotificadorEmail implements Notificador {
-
-    private String hostServidorSmtp;
-    private boolean caixaAlta;
-
-    public NotificadorEmail(String hostServidorSmtp) {
-        this.hostServidorSmtp = hostServidorSmtp;
-        System.out.println("Notificador E-mail " + this);
-    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        if(caixaAlta)
-            mensagem = mensagem.toUpperCase();
-
-        System.out.printf("Notificando %s através do e-mail %s usando SMTP: %s: %s\n",
-                cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
-    }
-
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
 
--- src/main/java/com/matheusfr/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/matheusfr/algafood/di/service/AtivacaoClienteService.java
@@ -2,18 +2,24 @@ package com.matheusfr.algafood.di.service;
 
 import com.matheusfr.algafood.di.modelo.Cliente;
 import com.matheusfr.algafood.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
+@Component
 public class AtivacaoClienteService {
 
+    @Autowired(required = false)
     private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-        System.out.println("AtivacaoClienteService" + notificador);
-    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
+
+        if(Objects.isNull(notificador)) {
+            System.out.println("Não existe notificador, mas cliente foi ativado!");
+            return;
+        }
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
