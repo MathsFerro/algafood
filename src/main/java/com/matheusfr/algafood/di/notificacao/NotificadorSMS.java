b878a2ccf8409d2567960cf0ff31448c08c4b0a4
Matheus Ferro <matheusferroraimundo@gmail.com>
2022-03-05 17:56:06 -0300

    2.16 Ambiguidade de beans e injeção de listas de beans, resolvendo com multiples beans

---
+++ src/main/java/com/matheusfr/algafood/di/notificacao/NotificadorSMS.java
@@ -0,0 +1,15 @@
+package com.matheusfr.algafood.di.notificacao;
+
+import com.matheusfr.algafood.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorSMS implements Notificador {
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
+                cliente.getNome(), cliente.getTelefone(), mensagem);
+    }
+}
+
--- src/main/java/com/matheusfr/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/matheusfr/algafood/di/service/AtivacaoClienteService.java
@@ -5,23 +5,26 @@ import com.matheusfr.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Objects;
 
 @Component
 public class AtivacaoClienteService {
 
     @Autowired(required = false)
-    private Notificador notificador;
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        if(Objects.isNull(notificador)) {
-            System.out.println("Não existe notificador, mas cliente foi ativado!");
-            return;
-        }
+        for (Notificador notificador : notificadores) {
+            if(Objects.isNull(notificador)) {
+                System.out.println("Não existe notificador, mas cliente foi ativado!");
+                continue;
+            }
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        }
     }
 
 }
